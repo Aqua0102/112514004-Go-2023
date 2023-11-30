530565ac1ac498d064f44f96b33f524a0d0d3740
Aqua0102 <catcat5499@gmail.com>
2023-11-30 15:14:54 +0800

    11/30 2

--- lab06/wasm/main.go
+++ lab06/wasm/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"math/big"
 	"syscall/js"
 )
 
-func main() {
+func registerCallBacks() {
 	js.Global().Set("CheckPrime", js.FuncOf(CheckPrime))
+}
+func main() {
+	registerCallBacks()
 
 	select {}
 }
@@ -16,19 +20,28 @@ func CheckPrime(this js.Value, p []js.Value) interface{} {
 		return js.ValueOf("Invalid argument count")
 	}
 
-	numStr := p[0].String()
+	js.Global().Get("document").Call("getElementById", "answer").Set("innerText", "test")
+	numStr := js.Global().Get("document").Call("getElementById", "value").Get("value").String()
 	num, ok := new(big.Int).SetString(numStr, 10)
 	if !ok {
+		updateAnswer("Invalid argument type")
 		return js.ValueOf("Invalid argument type")
 	}
-
+	fmt.Printf(numStr)
 	result := isPrime(num)
+
+	if result {
+		updateAnswer("It's prime")
+	} else {
+		updateAnswer("It's not prime")
+	}
 
 	if result {
 		return js.ValueOf("It's prime")
 	} else {
 		return js.ValueOf("It's not prime")
 	}
+
 }
 
 func isPrime(n *big.Int) bool {
@@ -37,4 +50,8 @@ func isPrime(n *big.Int) bool {
 	}
 
 	return n.ProbablyPrime(0)
+}
+
+func updateAnswer(answer string) {
+	js.Global().Get("document").Call("getElementById", "answer").Set("innerText", answer)
 }
